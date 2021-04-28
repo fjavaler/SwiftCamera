63242ae4a3cb1294ab3d473a531e8211ac3a16c7
Frederick Javalera <frederickjavalera@gmail.com>
2021-04-28 09:58:17 -0600

    Initial Commit

---
+++ SwiftCamera/SwiftCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCameraApp.swift
+//  SwiftCamera
+//
+//  Created by Fred Javalera on 4/28/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
